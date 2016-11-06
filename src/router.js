5624039c509209ac8da6a759029fa6c45e83e075
burning <iburning@live.cn>
2016-11-06 18:27:14 +0800

    sub router

--- src/router.js
+++ src/router.js
@@ -7,11 +7,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from './home.vue'
+import User from './user.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', component: Home },
+  // { path: '/user', component: User },
+  {
+    path: '/user/:user_id',
+    component: User,
+    children: [
+      {
+        path: 'post/:post_id',
+        component: { template: '<div>post {{ $route.params.id}}</div>' }
+      },
+      {
+        path: 'post/:post_id',
+        component: { template: '<div>post {{ $route.params.id}}</div>' }
+      },
+      {
+        path: 'follow',
+        component: { template: '<div>user follow</div>' }
+      }
+    ]
+  }
   // { path: '/foo', component: Foo },
   // { path: '/bar', component: Bar }
 ]
---
+++ src/user.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="user">
+    <h1>User {{ $route.params.user_id }}</h1>
+    <p>
+      <router-link to="/user/100/fans">Fans</router-link>
+      <router-link to="/user/100/follow">Follow</router-link>
+    </p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user'
+}
+</script>
